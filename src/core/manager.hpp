d421f106d7dde029c912954936aeac7caa34cf2e
Uoc <vanssenlingga20@gmail.com>
2025-10-24 23:00:28 +0000

    update header manager.hpp

--- src/core/manager.hpp
+++ src/core/manager.hpp
@@ -7,6 +7,10 @@ class PackageManager {
 public:
     void install(const std::string& name);
     void remove(const std::string& name);
+    void show(const std::string& name);
+    void list();
+    void dir();
+    void autoremove();
     void sync(const std::string& name);
     void syncAll();
     void showVersion();
@@ -18,7 +22,7 @@ private:
     void downloadFile(const std::string& url, const std::string& output);
     void extractPackage(const std::string& file, const std::string& dest);
     void showProgress(const std::string& pkg, int percent, const std::string& state);
-    std::string getSpeed();
     nlohmann::json getJSON(const std::string& url);
-    bool hasVersion(const std::string& name);
+    std::string humanSize(double bytes);
+    bool confirmAction(const std::string& msg);
 };
