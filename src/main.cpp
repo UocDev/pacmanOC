fb7f90016c2d8e9c1dea60f60354b2290a0346ee
Uoc <vanssenlingga20@gmail.com>
2025-10-24 22:59:18 +0000

    update main.cpp support new command dir, autoremove, show, and ls

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 
 int main(int argc, char* argv[]) {
     if (argc < 2) {
-        std::cout << "Usage: pacmanoc [install|remove|-s|-S|-v] <package>\n";
+        std::cout << "Usage: pacmanoc [install|remove|show|ls|dir|autoremove|-s|-S|-v] <package>\n";
         return 0;
     }
 
@@ -14,6 +14,14 @@ int main(int argc, char* argv[]) {
         mgr.install(argv[2]);
     else if ((cmd == "remove" || cmd == "uninstall") && argc > 2)
         mgr.remove(argv[2]);
+    else if (cmd == "show" && argc > 2)
+        mgr.show(argv[2]);
+    else if (cmd == "ls" || cmd == "list")
+        mgr.list();
+    else if (cmd == "dir")
+        mgr.dir();
+    else if (cmd == "autoremove")
+        mgr.autoremove();
     else if (cmd == "-s" && argc > 2)
         mgr.sync(argv[2]);
     else if (cmd == "-S")
