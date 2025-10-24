c7be07f4b72b96dc12ac0ed6205fd888682e3d59
Uoc <vanssenlingga20@gmail.com>
2025-10-24 09:47:56 +0000

    create db.hpp

---
+++ src/core/db.hpp
@@ -0,0 +1,19 @@
+#pragma once
+#include <string>
+#include <unordered_map>
+#include <nlohmann/json.hpp>
+
+class Database {
+private:
+    std::string dbPath = "/usr/local/share/pacmanoc/db.json";
+    std::unordered_map<std::string, nlohmann::json> installed;
+public:
+    void load();
+    void save();
+    bool isInstalled(const std::string& name);
+    void addPackage(const std::string& name, const std::string& version, const std::string& dest);
+    void removePackage(const std::string& name);
+    std::string getVersion(const std::string& name);
+    std::string getDestination(const std::string& name);
+    std::unordered_map<std::string, nlohmann::json> listInstalled();
+};
